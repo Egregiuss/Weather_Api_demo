de00641805867eab35382f55ccbc9198508e1af7
Ayobami Olaniyi <daegregius@gmail.com>
2025-05-18 15:20:32 +0100

    fix in the yaml file

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from weather.fetcher import get_weather
 
 if __name__ == "__main__":
     city = input("Enter city: ")
-    api_key = os.getenv("WEATHER_API_KEY")  # set this in your GitHub Secrets
+    api_key = os.getenv("WEATHER_API_KEY")  
     weather = get_weather(city, api_key)
     print(f"{weather['city']}: {weather['temp']}°C, {weather['description']}")
